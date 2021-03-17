5e6b026a2c145f04b9da131f505ebad950886b4c
Luis Biedma <lbiedma17@gmail.com>
2021-03-17 10:46:58 -0300

    comenzando

---
+++ codigos/prueba.py
@@ -0,0 +1,12 @@
+x = int(input("Ingrese un numero: "))
+y = int(input("Ingrese un numero: "))
+z = int(input("Ingrese un numero: "))
+# aca tengo que sumar los 3 numeros (no olvidarme de las parentesis) y dividir por 3
+
+"""
+hola
+esto es un
+comentario en bloque
+"""
+
+print("Si hago el promedio de los 3 numeros, voy a obtener ", (x + y + z) / 3)
